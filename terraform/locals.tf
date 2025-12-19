a78102d0a3a61f00811fff96a0f2b6b785296adc
Paul Kirk <pikirk@Pauls-MacBook-Pro.local>
2025-12-19 13:07:33 -0800

    fix lint honks

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -10,10 +10,6 @@ locals {
   # unobtanium/core/{env}/{stack_group}/{stack}/terraform.tfstate
   stack_group = "lambda-functions"
   stack_name  = "engraver-handler"
-  state_key   = "${local.org}/${local.domain}/${local.env}/${local.stack_group}/${local.stack_name}/terraform.tfstate"
-
-  # KMS per env (alias includes env suffix)
-  kms_alias_name = "alias/terraform-tfstate-${local.domain}-${local.env}"
 
   tags = {
     org        = local.org
